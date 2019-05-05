6b662d554a207434eb7217016dbb615d296f776e
ApoorvaJu08 <apoorva10339@gmail.com>
2019-05-05 15:24:09 +0530

    mcm

---
+++ mcm.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <limits.h>
+int mcm(int p[], int n)
+{
+    int m[n][n];
+    int i, j, k, l, q;
+    for(i = 1; i < n; i++)
+        m[i][i] = 0;
+    for(l = 2; l < n; l++)
+    {
+        for(i = 1; i < (n-l+1); i++)
+        {
+            j = (i+l-1);
+            m[i][j] = INT_MAX;
+            for(k = i; k <= (j-1); k++)
+            {
+                q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
+                if(q < m[i][j])
+                    m[i][j] = q;
+            }
+        }
+    }
+    return m[1][n-1];
+}
+
+int main()
+{
+    int n, i;
+    printf("Enter number of matrices: \n");
+    scanf("%d", &n);
+    n++;
+    int arr[n];
+    printf("Enter dimensions: \n");
+    for(i = 0; i < n; i++)
+    {
+        printf("Enter %d:", i);
+        scanf("%d", &arr[i]);
+    }
+    int size = sizeof(arr)/sizeof(arr[0]);
+    printf("Minimum number of multiplications are %d", mcm(arr, size));
+    return 0;
+}
